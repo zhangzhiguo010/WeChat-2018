dd2733364a297be59710bfcdcc10b013c9506462
zhangzhiguo <zhangzhiguo010@outlook.com>
2018-11-01 00:33:09 +0800

    Updates

--- precache-manifest.87624674d9831abc35d875a47f39a380.js
+++ precache-manifest.87624674d9831abc35d875a47f39a380.js
@@ -20,19 +20,19 @@ self.__precacheManifest = [
     "url": "/WeChat-2018/build/static/js/runtime~main.0eae0317.js"
   },
   {
-    "revision": "710d5c171753b1428fba",
-    "url": "/WeChat-2018/build/static/js/main.710d5c17.chunk.js"
+    "revision": "74b69817ba837869f684",
+    "url": "/WeChat-2018/build/static/js/main.74b69817.chunk.js"
   },
   {
     "revision": "9a9aebe0617f481c2799",
     "url": "/WeChat-2018/build/static/js/1.9a9aebe0.chunk.js"
   },
   {
-    "revision": "710d5c171753b1428fba",
+    "revision": "74b69817ba837869f684",
     "url": "/WeChat-2018/build/static/css/main.878f37e2.chunk.css"
   },
   {
-    "revision": "821a7ff3032bad4047f6e34fe35ad720",
+    "revision": "dea853451d1489ece5326c3e9a145db3",
     "url": "/WeChat-2018/build/index.html"
   }
 ];
